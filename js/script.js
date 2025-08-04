8b324b42e0fe8b3cf3f81154d733eb008276e219
Juan <anamarques1627@gmail.com>
2025-08-04 16:45:51 -0300

    Add: animação texto

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* zoom image */
 document.addEventListener("DOMContentLoaded", function () {
   const images = document.querySelectorAll('.zoom-image');
   const modal = document.getElementById('modal');
@@ -15,3 +16,18 @@ document.addEventListener("DOMContentLoaded", function () {
     modalImg.src = "";
   });
 });
+
+/* Texto vindo das laterais */
+document.addEventListener("DOMContentLoaded", function () {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target); 
+        }
+      });
+    }, { threshold: 0.1 });
+
+    const elements = document.querySelectorAll('.animate-right, .animate-left');
+    elements.forEach(el => observer.observe(el));
+  });
